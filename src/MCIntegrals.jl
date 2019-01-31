13049511070036625040492c37ae2b20514dd280
Jan Weidner <jw3126@gmail.com>
2019-01-31 22:07:20 +0100

    Add basic multi dimensional integration

--- src/MCIntegrals.jl
+++ src/MCIntegrals.jl
@@ -1,11 +1,12 @@
 module MCIntegrals
 
-export MCVanilla, Vegas
+export MCVanilla, Vegas, Domain
 export integral, ∫
 
 using QuickTypes
 using ArgCheck
 using LinearAlgebra, Statistics
+using StaticArrays
 
 abstract type MCAlgorithm end
 
@@ -13,32 +14,82 @@ abstract type MCAlgorithm end
     @argcheck neval > 2
 end <: MCAlgorithm
 
-function uniform(min, max)
-    min + rand() * (max - min)
+"""
+    Domain
+
+Encodes a domain of integration, i.e. a product of finte intervals.
+"""
+struct Domain{N,T}
+    lower::SVector{N,T}
+    upper::SVector{N,T}
 end
 
-function integral(f, domain, alg=Vegas)
-    integral_alg(f, domain, alg)
+function cartesian_product(d1::Domain, d2::Domain)
+    Domain(vcat(d1.lower, d2.lower), vcat(d1.upper, d2.upper))
 end
+
+function pointtype(::Type{Domain{N,T}}) where {N,T}
+    SVector{N,T}
+end
+
+function volume(dom::Domain)
+    prod(dom.upper - dom.lower)
+end
+
+function Domain(interval::NTuple{2,Number})
+    (a,b) = float.(promote(interval...))
+    Domain(@SVector[a], @SVector[b])
+end
+
+function Domain(dom::Domain)
+    dom
+end
+
+function Domain(lims)
+    lower = SVector(map(float ∘ first, lims))
+    upper = SVector(map(float ∘ last, lims))
+    Domain(lower, upper)
+end
+
+function uniform(dom::Domain)
+    V = pointtype(typeof(dom))
+    Δ = (dom.upper - dom.lower)
+    dom.lower .+ rand(V) .* Δ
+end
+
+function integral(f, domain, alg=Vegas())
+    integral_dom(f, domain, alg)
+end
+
+function integral_dom(f, dom, alg)
+    integral_alg(f, Domain(dom), alg)
+end
+
+function integral_dom(f, I::NTuple{2,Number}, alg)
+    f_v = f ∘ first
+    dom = Domain(I)
+    integral_alg(f_v, dom, alg)
+end
+
 const ∫ = integral
 
-function integral_alg(f, (a,b), alg::MCVanilla)
+function integral_alg(f, dom, alg::MCVanilla)
     N = alg.neval
-    x = uniform(a,b)
+    x = uniform(dom)
     y = f(x)
     sum = y
     sum2 = y^2
     for _ in 1:(N-1)
-        x = uniform(a,b)
+        x = uniform(dom)
         y = f(x)
         sum += y
         sum2 += y^2
     end
     
-    vol = b - a
+    vol = volume(dom)
     mean = sum/N
     var_f = (sum2/N) - mean^2
-    var_f = max(0., var_f)
+    var_f = max(zero(var_f), var_f)
     var_fmean = var_f / N
     std = sqrt(var_fmean)
     (value = mean*vol, std=std*vol)
@@ -51,9 +102,14 @@ end
     @argcheck neval > 2
 end <: MCAlgorithm
 
-@qstruct ImportanceQuantiles(positions::Vector{Float64}) do
-    @argcheck issorted(positions)
-    @argcheck length(positions) >= 2
+
+@qstruct Stratification{N,T}(
+    boundaries::NTuple{N,Vector{T}}
+   ) do
+    for i in 1:N
+        @argcheck length(boundaries[i]) >= 2
+        @argcheck issorted(boundaries[i])
+    end
 end
 
 @qstruct CDF(
@@ -82,9 +138,9 @@ function linterpol(x, (x_min, x_max), (y_min, y_max))
     end
 end
 
-function importance_quantiles(cdf::CDF, nquantiles)
+function importance_quantiles(cdf::CDF, nwalls)
     ret = [first(cdf.positions)]
-    qvals = range(first(cdf.values), stop=last(cdf.values), length=nquantiles)
+    qvals = range(first(cdf.values), stop=last(cdf.values), length=nwalls)
     i = 1
     for qval in qvals[2:end]
         while cdf.values[i] < qval
@@ -95,61 +151,106 @@ function importance_quantiles(cdf::CDF, nquantiles)
         q = linterpol(qval, xs, ys)
         push!(ret, q)
     end
-    @assert i == length(cdf.values)
-    @assert length(ret) == nquantiles
-    ImportanceQuantiles(ret)
+    # if i != length(cdf.values)
+    #     @show cdf.values
+    # end
+    # @assert i == length(cdf.values)
+    @assert length(ret) == nwalls
+    ret
 end
 
-function create_sample(iq::ImportanceQuantiles)
-    xs = iq.positions
-    inds = 1:length(xs) - 1
-    i = rand(inds)
-    x_lo = xs[i]
-    x_hi = xs[i+1]
-    Δ = x_hi - x_lo
-    x = x_lo + rand() * Δ
-    @assert x_lo <= x <= x_hi
-    # vol = last(xs) - first(xs)
-    # Δ_avg = vol 
-    wt = Δ #* (length(iq.positions) - 1)
-    (x=x, index=i, weight=wt)
+function Base.size(s::Stratification)
+    map(s.boundaries) do b
+        length(b) - 1
+    end
+end
+Base.length(s::Stratification) = prod(size(s))
+Base.eachindex(s::Stratification) = CartesianIndices(s)
+Base.axes(s::Stratification) = map(Base.OneTo, size(s))
+function Base.CartesianIndices(s::Stratification)
+    CartesianIndices(axes(s))
+end
+function Base.LinearIndices(s::Stratification)
+    LinearIndices(axes(s))
 end
 
-function tune(f, iq::ImportanceQuantiles; neval=1000, nquantiles=length(iq.positions))
-    xs = iq.positions
-    n = length(xs)
-    counts = fill(0, n-1)
-    sums = fill(0., n-1)
+function rand_index(s::Stratification)
+    li = rand(LinearIndices(s))
+    CartesianIndices(s)[li]
+end
+
+function Base.getindex(s::Stratification, index::CartesianIndex)
+    lower = map(s.boundaries, Tuple(index)) do walls, i
+        walls[i]
+    end
+    upper = map(s.boundaries, Tuple(index)) do walls, i
+        walls[i+1]
+    end
+    Domain(SVector(lower), SVector(upper))
+end
+
+function create_sample(s::Stratification)
+    i = rand_index(s)
+    cell = s[i]
+    x = uniform(cell)
+    # wt = volume(cell)
+    (x=x, index=i, cell=cell)
+end
+
+function estimate_cdf(histogram)
+    h = histogram
+    xs = h.walls
+    cdf_val = 0.
+    cdf_values = [cdf_val]
+    for i in eachindex(h.counts)
+        reg_sum   = 0. #1e-1*total_sum/n
+        reg_count = 0. #1
+        vol = xs[i+1] - xs[i]
+        cdf_val += h.sums[i] / h.counts[i]
+        push!(cdf_values, cdf_val)
+    end
+    CDF(xs, cdf_values)
+end
+
+function tune(f, iq::Stratification; neval=1000, outsize=size(iq))
+    hists = map(iq.boundaries) do xs
+        n = length(xs)
+        (
+            walls = xs,
+            counts = fill(0, n-1),
+            sums = fill(0., n-1),
+        )
+    end
+
     for _ in 1:neval
         s = create_sample(iq)
         y = norm(f(s.x))
-        counts[s.index] += 1
-        sums[s.index] += y*s.weight
+        cell = s.cell
+        for i in eachindex(iq.boundaries)
+            h = hists[i]
+            wt = cell.upper[i] - cell.lower[i]
+            h.counts[s.index[i]] += 1
+            h.sums[s.index[i]] += y*wt
+        end
     end
-    
-    cdf_val = 0.
-    cdf_values = [cdf_val]
-    for i in eachindex(counts)
-        reg_sum   = 0. #1e-1*total_sum/n
-        reg_count = 0. #1
-        vol = iq.positions[i+1] - iq.positions[i]
-        cdf_val += sums[i] / counts[i]
-        push!(cdf_values, cdf_val)
+
+    bdries_new = map(hists, outsize) do h, ncells
+        nwalls = ncells + 1
+        cdf = estimate_cdf(h)
+        importance_quantiles(cdf, nwalls)
     end
-    cdf = CDF(iq.positions, cdf_values)
-    importance_quantiles(cdf, nquantiles)
+
+    Stratification(bdries_new)
 end
 
-function integral_alg(f, iq::ImportanceQuantiles, alg::Vegas)
+function integral_alg(f, iq::Stratification, alg::Vegas)
     N = alg.neval
     sum = 0.
     sum2 = 0.
-    xs = iq.positions
-    inds = 1:length(xs)-1
-    vol = last(xs) - first(xs)
     for _ in 1:N
         s = create_sample(iq)
-        y = f(s.x) * s.weight * (length(iq.positions) - 1)
+        wt = volume(s.cell) * length(iq)
+        y = f(s.x) * wt
         sum += y
         sum2 += y^2
     end
@@ -161,11 +262,28 @@ function integral_alg(f, iq::ImportanceQuantiles, alg::Vegas)
     (value = mean, std=std)
 end
 
-function integral_alg(f, (a,b), alg::Vegas)
-    iq = ImportanceQuantiles([a,b])
-    iq = tune(f, iq, neval=200, nquantiles=10)
-    iq = tune(f, iq, neval=2000, nquantiles=100)
-    iq = tune(f, iq, neval=2000, nquantiles=100)
+function default_strat_size(dom::Domain{N}) where {N}
+    ntuple(_ -> 100, Val(N))
+end
+
+function equidistant_stratification(
+        dom::Domain, 
+        size=default_strat_size(dom)
+    )
+
+    bdries = map(Tuple(dom.lower), Tuple(dom.upper), size) do lo, hi, nbins
+        nwalls = nbins + 1
+        r = range(lo, stop=hi, length=nwalls)
+        collect(r)
+    end
+    Stratification(bdries)
+end
+
+function integral_alg(f, dom::Domain, alg::Vegas)
+    iq = equidistant_stratification(dom)
+    iq = tune(f, iq, neval=2000)
+    iq = tune(f, iq, neval=2000)
+    iq = tune(f, iq, neval=2000)
     integral_alg(f, iq, alg)
 end
 
--- test/runtests.jl
+++ test/runtests.jl
@@ -1,5 +1,7 @@
 using MCIntegrals
 using Test
+using StaticArrays
+using LinearAlgebra
 
 function isconsistent(truth, est; nstd=6, kw_approx...)
     val = est.value
@@ -11,6 +13,35 @@ function isconsistent(truth, est; nstd=6, kw_approx...)
     end
 end
 
+@testset "constant $(alg)" for alg in [
+    Vegas(10^2),
+    MCVanilla(10^2),
+    ]
+    for dim in 1:4
+        for _ in 1:10
+            lower = @SVector randn(dim)
+            upper = lower + @SVector rand(dim)
+            val = randn()
+            f = _ -> val
+            dom = Domain(lower, upper)
+            est = ∫(f, dom, alg)
+            vol = prod(upper .- lower)
+            truth = val * vol
+            @test isconsistent(truth, est)
+            @test est.std < 1e-2
+        end
+    end
+end
+
+@testset "∫x^2" begin
+    est_vegas = ∫(x->x^2, (0,1), Vegas(10000))
+    @test isconsistent(1/3, est_vegas)
+    @test est_vegas.std < 1e-3
+
+    est_vanilla = ∫(x->x^2, (0,1), MCVanilla(1000))
+    @test isconsistent(1/3, est_vanilla)
+    @test est_vanilla.std < 1e-2
+end
 
 @testset "∫(f, (a,b), $alg)" for alg in [
         MCVanilla(10^4),
@@ -34,5 +65,43 @@ end
             truth = F(b) - F(a)
             @test isconsistent(truth, est)
         end
+    end
+end
+
+@testset "Ball $alg" for alg in [
+    MCVanilla(10^4),
+    Vegas(10^4)]
+
+    f(x) = norm(x) < 1 ? 1. : 0.
+    est = ∫(f, (-2,2), alg)
+    @test isconsistent(2,est)
+
+    est = ∫(f, ((-1,1),(-1,1)), alg)
+    @test isconsistent(π, est)
+
+    est = ∫(f, ((-1,1),(-1,1),(-1,1)), alg)
+    @test isconsistent(4π/3, est)
+end
+
+
+@testset "Fubini $alg" for alg in [
+    MCVanilla(10^4),
+    Vegas(10^4)]
+    for _ in 1:10
+        a1,a2 = randn(2)
+        b1 = a1 + 10rand()
+        b2 = a2 + 10rand()
+        dom1 = Domain((a1, b1))
+        dom2 = Domain((a2, b2))
+        dom = Domain(((a1, b1), (a2, b2)))
+        f1 = x -> cos(x[1])
+        F1 = x -> sin(x[1])
+        f2 = x -> x[1]^2
+        F2 = x -> x[1]^3/3
+        f  = v -> f1(v[1]) * f2(v[2])
+        est1 = ∫(f1, dom1, alg)
+        est2 = ∫(f2, dom2, alg)
+        est  = ∫(f , dom , alg)
+        @test isconsistent(est1.value*est2.value, est)
     end
 end
